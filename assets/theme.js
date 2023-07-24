0f5146abda4fa9cd381498b7ba6c578a85c241a4
yuricooke <yuri.cooke@ensineme.com.br>
2023-07-24 17:42:43 -0300

    estrutura completa

--- assets/theme.js
+++ assets/theme.js
@@ -96,9 +96,11 @@ function changeColor(elementId) {
 function filterCards(filter) {
   const cards = document.querySelectorAll('#cards .card');
 
+  
+
   cards.forEach(card => {
     if (filter === 'all' || card.parentNode.getAttribute('data-category') === filter) {
-      card.parentNode.style.display = 'block';
+      card.parentNode.style.display = 'flex';
     } else {
       card.parentNode.style.display = 'none';
     }
